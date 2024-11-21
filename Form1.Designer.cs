bd380d6414cb2bc4ca8bf063ccd9719784a0bc6c
Azael <azfajardo69@gmail.com>
2024-11-21 17:11:14 -0600

    Mi primera chamba (agregar el proyecto commit)

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace _1._1__Tabla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.La_de_milanesa = new System.Windows.Forms.Label();
+            this.Cubana = new System.Windows.Forms.Label();
+            this.Española = new System.Windows.Forms.TextBox();
+            this.Boton_ajajaajja = new System.Windows.Forms.Button();
+            this.La_caja_rica_de_texto = new System.Windows.Forms.RichTextBox();
+            this.SI = new System.Windows.Forms.Label();
+            this.Juan_el_de_las_Tortas = new System.Windows.Forms.Panel();
+            this.Juan_el_de_las_Tortas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // La_de_milanesa
+            // 
+            this.La_de_milanesa.AutoSize = true;
+            this.La_de_milanesa.Font = new System.Drawing.Font("DejaVu Sans", 15.85714F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.La_de_milanesa.Location = new System.Drawing.Point(3, 9);
+            this.La_de_milanesa.Name = "La_de_milanesa";
+            this.La_de_milanesa.Size = new System.Drawing.Size(495, 43);
+            this.La_de_milanesa.TabIndex = 0;
+            this.La_de_milanesa.Text = "TABLAS DE MULTIPLICAR";
+            // 
+            // Cubana
+            // 
+            this.Cubana.AutoSize = true;
+            this.Cubana.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cubana.Location = new System.Drawing.Point(5, 52);
+            this.Cubana.Name = "Cubana";
+            this.Cubana.Size = new System.Drawing.Size(264, 36);
+            this.Cubana.TabIndex = 1;
+            this.Cubana.Text = "-Elegi una tabla po";
+            this.Cubana.Click += new System.EventHandler(this.Cubana_Click);
+            // 
+            // Española
+            // 
+            this.Española.Location = new System.Drawing.Point(11, 102);
+            this.Española.Name = "Española";
+            this.Española.Size = new System.Drawing.Size(100, 29);
+            this.Española.TabIndex = 2;
+            // 
+            // Boton_ajajaajja
+            // 
+            this.Boton_ajajaajja.AutoSize = true;
+            this.Boton_ajajaajja.Location = new System.Drawing.Point(11, 156);
+            this.Boton_ajajaajja.Name = "Boton_ajajaajja";
+            this.Boton_ajajaajja.Size = new System.Drawing.Size(100, 37);
+            this.Boton_ajajaajja.TabIndex = 3;
+            this.Boton_ajajaajja.Text = "Ver tabla";
+            this.Boton_ajajaajja.UseVisualStyleBackColor = true;
+            this.Boton_ajajaajja.Click += new System.EventHandler(this.Boton_ajajaajja_Click);
+            // 
+            // La_caja_rica_de_texto
+            // 
+            this.La_caja_rica_de_texto.Font = new System.Drawing.Font("Mongolian Baiti", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.La_caja_rica_de_texto.Location = new System.Drawing.Point(431, 102);
+            this.La_caja_rica_de_texto.Name = "La_caja_rica_de_texto";
+            this.La_caja_rica_de_texto.Size = new System.Drawing.Size(202, 268);
+            this.La_caja_rica_de_texto.TabIndex = 4;
+            this.La_caja_rica_de_texto.Text = "";
+            // 
+            // SI
+            // 
+            this.SI.AutoSize = true;
+            this.SI.Font = new System.Drawing.Font("Comic Sans MS", 8.142858F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SI.Location = new System.Drawing.Point(0, 397);
+            this.SI.Name = "SI";
+            this.SI.Size = new System.Drawing.Size(416, 29);
+            this.SI.TabIndex = 5;
+            this.SI.Text = "ANGEL AZAEL FAJARDO ESPINO ISC 3C";
+            this.SI.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Juan_el_de_las_Tortas
+            // 
+            this.Juan_el_de_las_Tortas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Juan_el_de_las_Tortas.Controls.Add(this.SI);
+            this.Juan_el_de_las_Tortas.Controls.Add(this.La_caja_rica_de_texto);
+            this.Juan_el_de_las_Tortas.Controls.Add(this.Boton_ajajaajja);
+            this.Juan_el_de_las_Tortas.Controls.Add(this.Española);
+            this.Juan_el_de_las_Tortas.Controls.Add(this.Cubana);
+            this.Juan_el_de_las_Tortas.Controls.Add(this.La_de_milanesa);
+            this.Juan_el_de_las_Tortas.Location = new System.Drawing.Point(12, 12);
+            this.Juan_el_de_las_Tortas.Name = "Juan_el_de_las_Tortas";
+            this.Juan_el_de_las_Tortas.Size = new System.Drawing.Size(776, 426);
+            this.Juan_el_de_las_Tortas.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Juan_el_de_las_Tortas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Juan_el_de_las_Tortas.ResumeLayout(false);
+            this.Juan_el_de_las_Tortas.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label La_de_milanesa;
+        private System.Windows.Forms.Label Cubana;
+        private System.Windows.Forms.TextBox Española;
+        private System.Windows.Forms.Button Boton_ajajaajja;
+        private System.Windows.Forms.RichTextBox La_caja_rica_de_texto;
+        private System.Windows.Forms.Label SI;
+        private System.Windows.Forms.Panel Juan_el_de_las_Tortas;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media; // Necesario para reproducir el sonido
+
+namespace _1._1__Tabla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Cubana_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Boton_ajajaajja_Click(object sender, EventArgs e)
+        {
+            this.muestraTabla();
+        }
+
+        private void muestraTabla()
+        {
+            int n = Convert.ToInt32(this.Española.Text);
+            string tabla = "";
+
+            if (n != 29)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    tabla = tabla + n + " x " + i + " = " + (n * i) + "\n";
+                }
+                this.La_caja_rica_de_texto.AppendText(tabla);
+            }
+            else
+            {
+                
+                PictureBox easterEggImage = new PictureBox
+                {
+                    Image = Properties.Resources.amongus, 
+                    SizeMode = PictureBoxSizeMode.AutoSize,
+                    Location = new Point(10, 150) 
+                };
+                this.Controls.Add(easterEggImage);
+
+                
+                SoundPlayer soundPlayer = new SoundPlayer(Properties.Resources.Sus_sound); 
+                soundPlayer.Play();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
